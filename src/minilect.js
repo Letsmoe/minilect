38f17118a9c001f6395967bfd4f9b73060dbf2e1
Moritz Utcke <62291876+Letsmoe@users.noreply.github.com>
2021-07-19 17:36:16 +0200

    Update minilect.js

--- src/minilect.js
+++ src/minilect.js
@@ -1,16 +1,17 @@
 class selector {
     constructor(opts) {
         let _self = this;
-        this.opts = Object.assign({
-            onHoverNode: function(e) {},
-            onOpen: function(e) {},
-            onClose: function(e) {},
-            onChange: function(e) {},
+        _self.opts = Object.assign({
+            onHoverNode: function (e) {},
+            onOpen: function (e) {},
+            onClose: function (e) {},
+            onChange: function (e) {},
+            onToggleGroup: function (e) {},
             enableSearch: false,
             renewSearch: true,
             caseSensitive: false,
-            matchingFunction: function(match, search) {
-                var check_case = (x) => this.caseSensitive ? x : x.toLowerCase();
+            matchingFunction: function (match, search) {
+                var check_case = (x) => _self.caseSensitive ? x : x.toLowerCase();
                 return check_case(match.substring(0, search.length)) == check_case(search)
             },
             target: "",
@@ -20,50 +21,58 @@ class selector {
                 value: "v"
             }
         }, opts);
-        let o_tag = this.opts.shorthandDescriptors.option;
+        let o_tag = _self.opts.shorthandDescriptors.option,
+            g_tag = _self.opts.shorthandDescriptors.group;
 
-        this.session = Math.random().toString(36).substring(2, 15);
-        this.target = opts.target
+        _self.session = Math.random().toString(36).substring(2, 15);
+        _self.target = opts.target
         // Append the actual opener to the body.
-        this.opener = $(`<div class='custom_select_details' data-opener-for='${this.session}'>${this.target.html()}</div>`);
-        $(document.body).append(this.opener)
+        _self.opener = $(`<div class='custom_select_details' data-opener-for='${_self.session}'>${_self.target.html()}</div>`);
+        _self.opener.find(g_tag).on("click", function (e) {
+            if($(e.target).is(g_tag)) {
+                $(this).toggleClass("is_closed");
+                _self.opts.onToggleGroup({target: this, wasClosed: $(this).is(".is_closed")})
+            };
+        })
+        $(document.body).append(_self.opener)
         // Remove left over values from the select
-        this.target.prop("id", this.session).on("click", function(e) {
+        _self.target.prop("id", _self.session).on("click", function (e) {
             _self.toggle(e);
-        }).find("*").remove().addBack().append(`<div class='value_inner' data-selector-for="${this.session}"></div>`)
-        this.is_open = false;
-        this.selected = this.opener.find(`${this.opener.find("[selected]").length == 0 ? o_tag : "[selected]"}`).eq(0).addClass("active");
-        this.value = this.selected.attr("value") || this.selected.text();
-        this.change_to(this.selected.html())
-        this.all = this.opener.find(`${o_tag}:not([disabled])`);
+        }).find("*").remove().addBack().append(`<div class='value_inner' data-selector-for="${_self.session}"></div>`)
+        _self.is_open = false;
+        _self.selected = _self.opener.find(`${_self.opener.find("[selected]").length == 0 ? o_tag : "[selected]"}`).eq(0).addClass("active");
+        _self.value = _self.selected.attr("value") || _self.selected.text();
+        _self.change_to(_self.selected.html())
+        _self.all = _self.opener.find(`${o_tag}:not([disabled])`);
 
-        this.all.on("click", function(e) {
+        _self.all.on("click", function (e) {
             _self.change(e)
-        }).on("mouseenter", function(e) {
+        }).on("mouseenter", function (e) {
             _self.focus_node($(e.target))
         })
-        this.last_key_search = ["", 0, this.selected];
-        $(document.body).on("keydown", function(e) {
+        _self.last_key_search = ["", 0, _self.selected];
+        $(document.body).on("keydown", function (e) {
             if (_self.is_open) {
-                let find = `${o_tag}:visible:not([disabled])`;
-                if (e.key == "ArrowDown") {
+                let find = `${o_tag}:visible:not([disabled])`,
+                    k = e.key;
+                if (k == "ArrowDown") {
                     let next = _self.selected.nextAll(find).eq(0),
                         n = next.length == 0 ? _self.opener.find(`${o_tag}:first-of-type`) : next;
                     _self.focus_node(n)
                     _self.scrollTo(n)
-                } else if (e.key == "ArrowUp") {
+                } else if (k == "ArrowUp") {
                     let prev = _self.selected.prevAll(find).eq(0),
                         p = prev.length == 0 ? _self.opener.find(`${o_tag}:last-of-type`) : prev;
                     _self.focus_node(p)
                     _self.scrollTo(p)
-                } else if (e.key == "Enter") {
+                } else if (k == "Enter") {
                     _self.change({
                         target: _self.selected
                     })
-                } else if ((e.key.length == 1 || e.key == "Backspace") && _self.opts.enableSearch) {
+                } else if (k.length == 1 || k == "Backspace") {
                     let _l = _self.last_key_search;
-                    _l[1] = _l[0] == e.key ? _l[2] == _self.selected ? _l[1] + 1 : 0 : 0
-                    _l[0] = _self.opts.renewSearch ? e.key : e.key == "Backspace" ? _l[0].substr(0, _l[0].length - 1) : _l[0] + e.key;
+                    _l[1] = _l[0] == k ? _l[2] == _self.selected ? _l[1] + 1 : 0 : 0
+                    _l[0] = _self.opts.renewSearch ? k : k == "Backspace" ? _l[0].substr(0, _l[0].length - 1) : _l[0] + k;
                     !_self.opts.renewSearch ? $(`.value_inner[data-selector-for="${_self.session}"]`).attr("data-search-term", _l[0]) : "";
                     let found = _self.find(_l[0], _l[1]);
                     if (found && found[1]) {
@@ -73,7 +82,7 @@ class selector {
                     } else if (_self.opts.renewSearch) {
                         _l[0] = "";
                     }
-                } else if (e.key == "Escape") {
+                } else if (k == "Escape") {
                     _self.toggle({
                         target: _self.selected
                     })
@@ -85,8 +94,8 @@ class selector {
     find(key, offset = 0) {
         let all = this.opener.find(this.opts.shorthandDescriptors.option),
             json = Object.values(all).map(x => [x.innerText, x]).filter(a => a[0] && this.opts.matchingFunction(a[0], key))
-        all.show()
-        this.opts.renewSearch == false ? all.not(json.map(x => x[1])).hide() : "";
+        all.removeClass("hidden_by_search");
+        this.opts.renewSearch == false ? all.not(json.map(x => x[1])).addClass("hidden_by_search") : "";
         return json[offset];
     }
 
@@ -103,7 +112,7 @@ class selector {
         if (pos > this.opener.innerHeight() || st > pos) {
             this.opener.animate({
                 scrollTop: pos
-            }, 100)
+            }, 0)
         }
     }
 
@@ -138,16 +147,22 @@ class selector {
     }
 
     toggle(e) {
-        if (this.is_open) {
-            this.opener.hide();
-            this.target.removeClass("isOpen").addClass("isClosed")
-            this.opts.onClose(e.target);
+        let _self = this;
+        if (_self.is_open) {
+            _self.opener.hide();
+            _self.target.removeClass("isOpen").addClass("isClosed")
+            _self.opts.onClose(e.target);
         } else {
-            this.position_on()
-            this.target.removeClass("isClosed").addClass("isOpen")
-            this.opts.onOpen(e.target);
+            _self.position_on()
+            _self.target.removeClass("isClosed").addClass("isOpen")
+            _self.opts.onOpen(e.target);
         }
-        this.is_open = !this.is_open;
+        _self.is_open = !_self.is_open;
 
     }
 }
+let minilect = new selector({
+    target: $('.custom_select'),
+    renewSearch: false,
+    enableSearch: true
+})
